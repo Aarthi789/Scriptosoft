2665b9daba54bf61187ff5d6e186116cf6219165
Aarthi <aarthi@scriptosoft.com>
2022-12-08 23:52:25 +0530

    webdesign styles changes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {MatExpansionModule} from '@angular/material/expansion';
 
-
+const mat =[
+  MatExpansionModule
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [mat],
+  exports:[mat]
 })
 export class MaterialModule { }
