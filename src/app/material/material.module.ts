2f4c3c7a84d34207d9fb3ddd781ba00c6e569fd6
Aarthi <aarthi@scriptosoft.com>
2022-12-12 16:40:32 +0530

    webdesign component design changes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 const mat =[
-  MatExpansionModule
+  MatExpansionModule,MatFormFieldModule,MatInputModule
 ];
 
 @NgModule({
--- src/app/services/web-design/web-design.component.ts
+++ src/app/services/web-design/web-design.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatAccordion} from '@angular/material/expansion';
 @Component({
   selector: 'app-web-design',
   templateUrl: './web-design.component.html',
   styleUrls: ['./web-design.component.scss']
 })
 export class WebDesignComponent implements OnInit {
-
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
   constructor() { }
 
   ngOnInit(): void {
